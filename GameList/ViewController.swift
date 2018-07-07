3a686727e054969bf51847952c7bba4b2acbbc1a
Carlos M. Vicente Herrero <cmvicentehe@gmail.com>
2018-07-07 10:24:02 +0200

    Initial Commit

---
+++ GameList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameList
+//
+//  Created by Carlos Manuel Vicente Herrero on 7/7/18.
+//  Copyright © 2018 Carlos Manuel Vicente Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
